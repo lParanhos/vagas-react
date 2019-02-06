8dbc5ad526e98cbf38e5aa69fc93d3c90b1f5734
lParanhos <leandro-paranhos@hotmail.com>
2019-02-06 15:37:29 -0200

    Atualizações na criação de uma nova vaga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/navigation/Header/Header';
-import JobsList from './components/Job/JobsList';
+import JobsList from './components/JobManagement/JobManagement';
 import Form from './components/Form/Form';
 import Collapse from './components/navigation/Collapse/Collapse'
 class App extends Component {
@@ -12,9 +12,7 @@ class App extends Component {
                 <Header />
 
                 <div className="container pt-3">
-                <Collapse textButton="Criar Regra" collapseId="jobForm">
-                    <Form/>
-                </Collapse>
+                
                     <JobsList />
                 </div>
             </div>
